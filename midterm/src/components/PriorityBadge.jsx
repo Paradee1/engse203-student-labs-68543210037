4993ca728dcbd7a2ae2a31504b2dcee4bb98dc8a
Paradee1 <paradee5759@gmail.com>
2026-08-31 15:05:32 +0700

    B4.2: unknown case

--- midterm/src/components/PriorityBadge.jsx
+++ midterm/src/components/PriorityBadge.jsx
@@ -1,13 +1,13 @@
 function PriorityBadge({ priority }) {
   if (priority === 'urgent') {
-    return <span className="priority-urgent">เร่งด่วน</span>;
+    return <span className="badge priority-urgent">เร่งด่วน</span>;
   }
 
   if (priority === 'normal') {
-    return <span className="priority-normal">ปกติ</span>;
+    return <span className="badge priority-normal">ปกติ</span>;
   }
 
-  return null;
+  return <span className="badge priority-unknown">ไม่ระบุ</span>;
 }
 
 export default PriorityBadge;
